24af22c358a976463216b65ebe8390f70e47c9e2
brahmaiahcheerla01 <brahmaiahcheerla@gmail.com>
2025-11-06 16:28:46 +0530

    code of version 2 added to app.java

--- app.java
+++ app.java
@@ -10,3 +10,18 @@ class Main {
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
+
+//version 2
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
